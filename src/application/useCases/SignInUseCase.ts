e3e817064c7f9564baa2703d8a405949b9fe81b8
IanFsciety <iandelas123@outlook.com>
2025-03-16 19:10:40 -0400

    inject role to jwt payload

--- src/application/useCases/SignInUseCase.ts
+++ src/application/useCases/SignInUseCase.ts
@@ -5,35 +5,39 @@ import { InvalidCredentials } from '../errors/InvalidCredentials';
 import { env } from '../config/env';
 
 interface IInput {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 interface IOutput {
-  accessToken: string;
+	accessToken: string;
 }
 
 export class SignInUseCase {
-  async execute({ email, password }: IInput): Promise<IOutput> {
-    const account = await prismaClient.account.findUnique({
-      where: { email },
-      select: { id: true, password: true },
-    });
+	async execute({ email, password }: IInput): Promise<IOutput> {
+		const account = await prismaClient.account.findUnique({
+			where: { email },
+			select: { id: true, password: true, role: true },
+		});
 
-    if (!account) {
-      throw new InvalidCredentials();
-    }
+		if (!account) {
+			throw new InvalidCredentials();
+		}
 
-    const isPasswordValid = await compare(password, account.password);
+		const isPasswordValid = await compare(password, account.password);
 
-    if (!isPasswordValid) {
-      throw new InvalidCredentials();
-    }
+		if (!isPasswordValid) {
+			throw new InvalidCredentials();
+		}
 
-    const accessToken = sign({ sub: account.id }, env.jwtSecret, {
-      expiresIn: '7d',
-    });
+		const accessToken = sign(
+			{ sub: account.id, role: account.role },
+			env.jwtSecret,
+			{
+				expiresIn: '7d',
+			},
+		);
 
-    return { accessToken };
-  }
+		return { accessToken };
+	}
 }
